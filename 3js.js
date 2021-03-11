4626756ba03c99842b1b1caff957ed63875033d6
Joni Lee <62823188+ChillySandwich@users.noreply.github.com>
2021-03-12 11:07:18 +1300

    Update 3js.js

--- 3js.js
+++ 3js.js
@@ -10,8 +10,10 @@ var camera = new THREE.PerspectiveCamera
         0.1,
         1000)
 
+// alter z position of camera
 camera.position.z = 2.5;
 
+//create WebGL renderer
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor("#FFFFFF");//background color
 renderer.setSize(200, 200);
@@ -32,22 +34,26 @@ function resizeCanvasToDisplaySize() {
     }
 }
 
-
+// create texture
 var texture = new THREE.TextureLoader().load('header.png');
+//create flat plane to animate
 var geometry = new THREE.PlaneGeometry(5, 3, 30, 30);
+// create clock
 var clock = new THREE.Clock;
+//create mesh material, adding on texture
 var material = new THREE.MeshBasicMaterial({ map: texture });
-
+// create mesh combining flat plane and mesh material
 var flag = new THREE.Mesh(geometry, material);
 scene.add(flag);
 
 flag.rotation.set(-0.1, 0,0);
 
+// animate function
 var render = function () {
     const t = clock.getElapsedTime()
 
     resizeCanvasToDisplaySize();
-
+    //map each vertice from the plane. Create sin waves.
     flag.geometry.vertices.map(v => {
         const wave1 = 0.5 * Math.sin(v.x * 2 + t *0.9)
         const wave2 = 0.25 * Math.sin(v.x * 3 + t *2)
@@ -55,9 +61,9 @@ var render = function () {
 
         v.z = (wave1 + wave2) * multi
     })
-
+    //update each vertice
     flag.geometry.verticesNeedUpdate = true;
-
+    
     requestAnimationFrame(render)
     renderer.render(scene, camera);
 }
